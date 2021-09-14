f2167ab98145a5d75909152555cb4b61992f83cc
Dimitar Nikolov <dknikolov@gmail.com>
2021-09-14 14:06:44 +0300

    feat: add text field skeleton

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
-        </div>
-      </section>
-      <div class="container is-fullhd">
-        <div class="notification">
-          Edit the <code>./src</code> folder to add components.
-        </div>
+     <div className="control has-icons-right">
+        <input
+          className="input is-large"
+          type="text"
+          placeholder="Enter number..."
+
+        />
+        <span className="icon is-small is-right">
+          <i className="fas fa-times" />
+        </span> 
       </div>
     </div>
   );
