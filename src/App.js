3016c57c2e6084be515c9292f5afe79d9d14e0ce
emo_new_bug <emotupev11@gmail.com>
2023-02-12 00:43:26 +0200

    fix: useMemo hook sync

--- src/App.js
+++ src/App.js
@@ -4,33 +4,35 @@ import React, { useState, useMemo } from 'react';
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [isValidNumber, setValidNumber] = useState(false);
-  const isValid = useMemo(() => {
-    console.log(isValidNumber);
-    setValidNumber(isValidNumber)}, [isValidNumber]);
+
+
+
+  const checkIfValid = (e) => {
+    return !isNaN(e) && e !== "";
+  }
+
+  const isValid = useMemo(() =>
+    checkIfValid(inputValue)
+    , [inputValue]);
 
   const setValue = (e) => {
-    setInputValue();
-    checkIfValid(e);
-    }
-
-    const checkIfValid = (e) => {
-      !isNaN(e) && e !== "" ? setValidNumber(true) : setValidNumber(false);
-    }
-  
+    setInputValue(e);
+    setValidNumber(isValid);
+  }
 
   return (
     <div className="App">
-     <div className="control has-icons-right">
+      <div className="control has-icons-right">
         <input
-        onChange={(e) => setValue(e.target.value)}
+          onChange={(e) => setValue(e.target.value)}
           className="input is-large"
           type="text"
           placeholder="Enter number..."
 
         />
         <span className="icon is-small is-right">
-          <i className={`fas ${isValidNumber ? "fa-check" :"fa-times" }`} />
-        </span> 
+          <i className={`fas ${isValid ? "fa-check" : "fa-times"}`} />
+        </span>
       </div>
     </div>
   );
