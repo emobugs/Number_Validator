eb0f991c488c34737249271dd4693fe03c241820
emo_new_bug <emotupev11@gmail.com>
2023-02-11 23:57:28 +0200

    Revert "feat: add input ui response"
    This reverts commit e1d51b72bdda0c19da9a108a182b0d009e8610ba.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import "./App.css";
-import React, { useState, useMemo } from 'react';
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
-  const [isValidNumber, setValidNumber] = useState(false);
-  const isValid = useMemo(() => {
-    console.log(isValidNumber);
-    setValidNumber(isValidNumber)}, [isValidNumber]);
-
-  const setValue = (e) => {
-    setInputValue();
-    checkIfValid(e);
-    }
-
-    const checkIfValid = (e) => {
-      !isNaN(e) && e !== "" ? setValidNumber(true) : setValidNumber(false);
-    }
-  
-
   return (
     <div className="App">
      <div className="control has-icons-right">
         <input
-        onChange={(e) => setValue(e.target.value)}
           className="input is-large"
           type="text"
           placeholder="Enter number..."
 
         />
         <span className="icon is-small is-right">
-          <i className={`fas ${isValidNumber ? "fa-check" :"fa-times" }`} />
+          <i className="fas fa-times" />
         </span> 
       </div>
     </div>
